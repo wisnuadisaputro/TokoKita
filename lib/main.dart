d6d2d435f0adbd870a489c1a6ccb711f414cabbc
wisnuadisaputro <wisnuadisaputro521@gmail.com>
2022-11-28 21:51:43 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:tokokita/ui/login_page.dart';
+import 'package:tokokita/ui/registrasi_page.dart';
+import 'package:tokokita/ui/produk_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Toko Kita',
+      debugShowCheckedModeBanner: false,
+      home: ProdukPage(),
+    );
+  }
+}
